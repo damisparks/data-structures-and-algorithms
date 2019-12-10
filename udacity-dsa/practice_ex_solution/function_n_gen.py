b60300e6924e5609c0c390e7ab66880822ae4164
Damilola <omifaredammy@gmail.com>
2019-12-10 21:30:45 +0000

    Add Solution for Practice Exercise 1.

--- udacity-dsa/practice_ex_solution/function_n_gen.py
+++ udacity-dsa/practice_ex_solution/function_n_gen.py
@@ -1,1 +1,27 @@
 ## Solution for Practice Exercise 1. 
+def prod(a,b):
+    return a * b
+
+def fact_gen():
+    i = 1
+    n = i
+    while True:
+        output = prod(n, i)
+        yield output
+        i += 1
+        n = output
+
+# Test block
+my_gen = fact_gen()
+num = 5
+for i in range(num):
+    print(next(my_gen))
+
+# Correct result when num = 5:
+# 1
+# 2
+# 6
+# 24
+# 120
+
+
