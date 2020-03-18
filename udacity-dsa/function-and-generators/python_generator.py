54c3da7d920224003b6fcfe5200ae61b73946391
Damilola <omifaredammy@gmail.com>
2020-03-18 23:50:04 +0000

    docs : Restructure folder
    - Restructure project folder.
    - Add function-and-generator folder.
    - Add vs code settings.

---
+++ udacity-dsa/function-and-generators/python_generator.py
@@ -0,0 +1,40 @@
+# Solution for Practice Exercise 2
+correct = [[1, 2, 3],
+           [2, 3, 1],
+           [3, 1, 2]]
+
+incorrect = [[1, 2, 3, 4],
+             [2, 3, 1, 3],
+             [3, 1, 2, 3],
+             [4, 4, 4, 4]]
+
+incorrect2 = [[1, 2, 3, 4],
+              [2, 3, 1, 4],
+              [4, 1, 2, 3],
+              [3, 4, 1, 2]]
+
+incorrect3 = [[1, 2, 3, 4, 5],
+              [2, 3, 1, 5, 6],
+              [4, 5, 2, 1, 3],
+              [3, 4, 5, 2, 1],
+              [5, 6, 4, 3, 2]]
+
+incorrect4 = [['a', 'b', 'c'],
+              ['b', 'c', 'a'],
+              ['c', 'a', 'b']]
+
+incorrect5 = [[1, 1.5],
+              [1.5, 1]]
+
+# Define a function check_sudoku()
+
+
+def check_sudokuy():
+    """
+    Define a procedure check_sudoku that takes as input a square list of lists representing an n x n sudoku puzzle solution and returns the boolean True
+    If the input is a valid sudoku square and returns the boolean False otherwise.
+    A valid sudoku square satisfies these two properties:
+    - Each column of the square contains each of the whole numbers from 1 to n exactly once.
+    - Each row of the square contains each of the whole numbers from 1 to n exactly once.
+    """
+    return 'Work in Progress.'
